6b8f681db64d78ce46f7e50732d648caba2a5c1a
colingalvin <colingalvin@ymail.com>
2020-11-13 11:57:03 -0600

    Continue building onto game.py/game_search.html; add logic to combine stats for games on multiple consoles.

--- datatracker/game.py
+++ datatracker/game.py
@@ -16,19 +16,64 @@ def search_game():
     else:
         api_result = requests.get('https://api.dccresource.com/api/games')
         games = api_result.json()
-        chosen_game = request.form['title']
+        search_game_title = request.form['title']
 
-        for title in games:
-            if title['name'] == chosen_game:
-                return render_template('Views/game_search.html', title=title)
+        game_search_results = []
 
-        return render_template('Views/game_search.html', error_message=error_message)
+        for game in games:
+            # .contains for partial searches
+            if game['name'] == search_game_title:
+                game_search_results.append(game)
+
+        if len(game_search_results) == 1:
+            return render_template('Views/game_search.html', game=game_search_results[0])
+        elif len(game_search_results) > 1:
+            game_stats_combined = combine_game_stats(game_search_results)
+            return render_template('Views/game_search.html', game=game_stats_combined, game_by_console=game_search_results)
+        else:
+            return render_template('Views/game_search.html', error_message=error_message)
+
+
+
+
+def combine_game_stats(game_list):
+    game_combined = {
+        'name': game_list[0]['name'],
+        'year': game_list[0]['year'],
+        'platform': '',
+        'publisher': game_list[0]['publisher'],
+        'genre': game_list[0]['genre'],
+        'naSales': 0,
+        'euSales': 0,
+        'jpSales': 0,
+        'otherSales': 0,
+        'globalSales': 0
+    }
+    for current_game in game_list:
+        game_combined['platform'] += (current_game['platform']) + ', '
+        game_combined['naSales'] += current_game['naSales']
+        game_combined['euSales'] += current_game['euSales']
+        game_combined['jpSales'] += current_game['jpSales']
+        game_combined['otherSales'] += current_game['otherSales']
+        game_combined['globalSales'] += current_game['globalSales']
+
+    # remove extra ', ' from end of platform
+    game_combined['platform'] = game_combined['platform'][:-2]
+
+    # round all values to 2 decimals
+    game_combined['naSales'] = round(game_combined['naSales'], 2)
+    game_combined['euSales'] = round(game_combined['euSales'], 2)
+    game_combined['jpSales'] = round(game_combined['jpSales'], 2)
+    game_combined['otherSales'] = round(game_combined['otherSales'], 2)
+    game_combined['globalSales'] = round(game_combined['globalSales'], 2)
+    return game_combined
+
 
 @bp.route('/consoleProjection')
 def console_projection():
     api_result = requests.get('https://api.dccresource.com/api/games')
     games = api_result.json()
-    game_after2013=[]
+    game_after2013 = []
     for game in games:
         if game['year'] is None:
             continue
@@ -50,5 +95,30 @@ def console_projection():
     _ds_sales = 0
     _psp_sales = 0
     for current_game in game_after2013:
-        switcher
-    return render_template('Views/console_projection.html', console=console)
+        if current_game['platform'] == "PS3":
+            _ps3_sales += current_game['globalSales']
+        elif current_game['platform'] == "X360":
+            _x360_sales += current_game['globalSales']
+        elif current_game['platform'] == "3DS":
+            _3ds_sales += current_game['globalSales']
+        elif current_game['platform'] == "PS4":
+            _ps4_sales += current_game['globalSales']
+        elif current_game['platform'] == "XOne":
+            _xone_sales += current_game['globalSales']
+        elif current_game['platform'] == "WiiU":
+            _wiiu_sales += current_game['globalSales']
+        elif current_game['platform'] == "Wii":
+            _wii_sales += current_game['globalSales']
+        elif current_game['platform'] == "PC":
+            _pc_sales += current_game['globalSales']
+        elif current_game['platform'] == "PSV":
+            _psv_sales += current_game['globalSales']
+        elif current_game['platform'] == "DS":
+            _ds_sales += current_game['globalSales']
+        elif current_game['platform'] == "PSP":
+            _psp_sales += current_game['globalSales']
+    sales_by_console_totals = [round(_ps3_sales, 2), round(_x360_sales, 2), round(_3ds_sales, 2), round(_ps4_sales, 2),
+                               round(_xone_sales, 2), round(_wiiu_sales, 2), round(_wii_sales, 2), round(_pc_sales, 2),
+                               round(_psv_sales, 2), round(_ds_sales, 2), round(_psp_sales, 2)]
+
+    return render_template('Views/console_projection.html', console=console, sales=sales_by_console_totals)
