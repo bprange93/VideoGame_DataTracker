273e1b50370cb1c1d64cfcdbd5e308a4c8d1c527
Brandon Prange <bprange93@gmail.com>
2020-11-13 10:08:31 -0600

    Add consoles to list. Start work on search to determine the best console.

--- datatracker/game.py
+++ datatracker/game.py
@@ -23,3 +23,32 @@ def search_game():
                 return render_template('Views/game_search.html', title=title)
 
         return render_template('Views/game_search.html', error_message=error_message)
+
+@bp.route('/consoleProjection')
+def console_projection():
+    api_result = requests.get('https://api.dccresource.com/api/games')
+    games = api_result.json()
+    game_after2013=[]
+    for game in games:
+        if game['year'] is None:
+            continue
+        elif game['year'] >= 2013:
+            game_after2013.append(game)
+    console = []
+    for game in game_after2013:
+        if game['platform'] not in console:
+            console.append(game['platform'])
+    _ps3_sales = 0
+    _x360_sales = 0
+    _3ds_sales = 0
+    _ps4_sales = 0
+    _xone_sales = 0
+    _wiiu_sales = 0
+    _wii_sales = 0
+    _pc_sales = 0
+    _psv_sales = 0
+    _ds_sales = 0
+    _psp_sales = 0
+    for current_game in game_after2013:
+        switcher
+    return render_template('Views/console_projection.html', console=console)
