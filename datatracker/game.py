0b43592e9c1f605ad1d0ff604b42564c5184a5c2
colingalvin <colingalvin@ymail.com>
2020-11-16 15:31:05 -0600

    MVP Complete; general cleaning.

--- datatracker/game.py
+++ datatracker/game.py
@@ -21,7 +21,6 @@ def search_game():
         game_search_results = []
 
         for game in games:
-            # .contains for partial searches
             if game['name'] == search_game_title:
                 game_search_results.append(game)
 
@@ -38,67 +37,18 @@ def search_game():
 def console_projection():
     api_result = requests.get('https://api.dccresource.com/api/games')
     games = api_result.json()
-    game_after2013 = []
-    for game in games:
-        if game['year'] is None:
-            continue
-        elif game['year'] >= 2013:
-            game_after2013.append(game)
-    console = []
-    for game in game_after2013:
-        if game['platform'] not in console:
-            console.append(game['platform'])
-    _ps3_sales = 0
-    _x360_sales = 0
-    _3ds_sales = 0
-    _ps4_sales = 0
-    _xone_sales = 0
-    _wiiu_sales = 0
-    _wii_sales = 0
-    _pc_sales = 0
-    _psv_sales = 0
-    _ds_sales = 0
-    _psp_sales = 0
-    for current_game in game_after2013:
-        if current_game['platform'] == "PS3":
-            _ps3_sales += current_game['globalSales']
-        elif current_game['platform'] == "X360":
-            _x360_sales += current_game['globalSales']
-        elif current_game['platform'] == "3DS":
-            _3ds_sales += current_game['globalSales']
-        elif current_game['platform'] == "PS4":
-            _ps4_sales += current_game['globalSales']
-        elif current_game['platform'] == "XOne":
-            _xone_sales += current_game['globalSales']
-        elif current_game['platform'] == "WiiU":
-            _wiiu_sales += current_game['globalSales']
-        elif current_game['platform'] == "Wii":
-            _wii_sales += current_game['globalSales']
-        elif current_game['platform'] == "PC":
-            _pc_sales += current_game['globalSales']
-        elif current_game['platform'] == "PSV":
-            _psv_sales += current_game['globalSales']
-        elif current_game['platform'] == "DS":
-            _ds_sales += current_game['globalSales']
-        elif current_game['platform'] == "PSP":
-            _psp_sales += current_game['globalSales']
-    sales_by_console_totals = [round(_ps3_sales, 2), round(_x360_sales, 2), round(_3ds_sales, 2), round(_ps4_sales, 2),
-                               round(_xone_sales, 2), round(_wiiu_sales, 2), round(_wii_sales, 2), round(_pc_sales, 2),
-                               round(_psv_sales, 2), round(_ds_sales, 2), round(_psp_sales, 2)]
-    return render_template('Views/consoleProjection.html', console=console, sales=sales_by_console_totals)
+    games_after_2013 = games_after_date(games, 2013)
+    consoles = get_consoles(games_after_2013)
+    console_totals = get_console_totals(games_after_2013, consoles)
+    return render_template('Views/consoleProjection.html', labels=consoles, data=console_totals)
 
 
 @bp.route('/sampleQuestion')
 def plot_game_stats():
     api_result = requests.get('https://api.dccresource.com/api/games')
     games = api_result.json()
-
-    # returns list of unique genres
     genres = get_genres(games)
-
-    # returns list of totals by genre
     genre_totals = get_genre_totals(games, genres)
-
     return render_template('Views/sampleQuestion.html', data=genre_totals, labels=genres)
 
 
@@ -106,21 +56,25 @@ def plot_game_stats():
 def plot_bonus_question():
     api_result = requests.get('https://api.dccresource.com/api/games')
     games = api_result.json()
-
-    #list of consoles
     consoles = get_consoles(games)
-    # separate games by console - list of consoles
     games_by_console = sort_games_by_console(games, consoles)
-
-    # separate console lists by publisher
     games_by_console_and_publisher = sort_games_by_publisher(games_by_console)
 
     return render_template('Views/bonusQuestion.html', dataset=games_by_console_and_publisher, labels=consoles)
 
 
+def games_after_date(games, date):
+    games_filtered = []
+    for game in games:
+        if game['year'] is None:
+            continue
+        elif game['year'] >= date:
+            games_filtered.append(game)
+    return games_filtered
+
+
 def sort_games_by_publisher(games_by_console):
     games_by_console_and_publisher = {}
-    # iterate over dictionary
     for console in games_by_console:
         games_by_console_and_publisher[console] = {}
     for console, games in games_by_console.items():
@@ -129,10 +83,7 @@ def sort_games_by_publisher(games_by_console):
                 games_by_console_and_publisher[game['platform']][game['publisher']] = 1
             elif game['publisher'] in games_by_console_and_publisher[game['platform']]:
                 games_by_console_and_publisher[game['platform']][game['publisher']] += 1
-
     return games_by_console_and_publisher
-    
-    # goal data structure: games_by_console{platforms{publisher: total_games}}
 
 
 def sort_games_by_console(games, consoles):
@@ -163,12 +114,24 @@ def get_consoles(games):
     return consoles
 
 
+def get_console_totals(games, consoles):
+    console_totals = []
+    for console in consoles:
+        console_totals.append(0)
+    for game in games:
+        for index, console in enumerate(consoles):
+            if game['platform'] == console:
+                console_totals[index] += game['globalSales']
+                break
+    for index, total in enumerate(console_totals):
+        console_totals[index] = round(total, 2)
+    return console_totals
+
+
 def get_genre_totals(games, genres):
     genre_totals = []
-
     for genre in genres:
         genre_totals.append(0)
-
     for game in games:
         for index, genre in enumerate(genres):
             if game['genre'] == genre:
@@ -186,10 +149,8 @@ def get_publishers(games):
 
 def get_publisher_totals(games, publishers):
     publisher_totals = []
-
     for publisher in publishers:
         publisher_totals.append(0)
-
     for game in games:
         for index, publisher in enumerate(publishers):
             if game['publisher'] == publisher:
@@ -217,11 +178,7 @@ def combine_game_stats(game_list):
         game_combined['jpSales'] += current_game['jpSales']
         game_combined['otherSales'] += current_game['otherSales']
         game_combined['globalSales'] += current_game['globalSales']
-
-    # remove extra ', ' from end of platform
     game_combined['platform'] = game_combined['platform'][:-2]
-
-    # round all values to 2 decimals
     game_combined['naSales'] = round(game_combined['naSales'], 2)
     game_combined['euSales'] = round(game_combined['euSales'], 2)
     game_combined['jpSales'] = round(game_combined['jpSales'], 2)
